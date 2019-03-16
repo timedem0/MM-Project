385726a9f5af97a927931661be81a0ad5a468c9c
Tudor <timedemo@gmail.com>
2019-03-16 16:58:10 +0200

    Add Firebase Listener to Game Component

--- components/Game.js
+++ components/Game.js
@@ -7,9 +7,8 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      /*currentUser: null,*/
+      currentUser: null,
       p1Email: '', p1Games: 0, p1Wins: 0, p1Draws: 0, p1Choice: 0,
-      /*searching: 0,*/
       p2Email: '', p2Games: 0, p2Wins: 0, p2Draws: 0, p2Choice: 0,
     };
   }
@@ -26,27 +25,39 @@ export default class Game extends React.Component {
 
   getUsersData = () => {
 
-    userOne = firebase.auth().currentUser.uid;
-    firebase.database().ref('/users/' + userOne).once('value', snapshot => {
-      const userOneDetails = snapshot.val();
-      this.setState({ p1Email: userOneDetails.email, p1Games: userOneDetails.games, p1Wins: userOneDetails.wins, p1Draws: userOneDetails.draws });
-    });
-
     const { navigation } = this.props;
+    const userOne = firebase.auth().currentUser;
     const userTwo = navigation.getParam('data', 'NO-ID');
-    firebase.database().ref('/users/' + userTwo).once('value', snapshot => {
-      const userTwoDetails = snapshot.val();
-      this.setState({ p2Email: userTwoDetails.email, p2Games: userTwoDetails.games, p2Wins: userTwoDetails.wins, p2Draws: userTwoDetails.draws });
-    });
+
+    firebase
+      .database()
+      .ref('/users/' + userOne.uid)
+      .on('value', snapshot => {
+        const userOneDetails = snapshot.val();
+        this.setState({ currentUser: userOne, p1Email: userOneDetails.email, p1Games: userOneDetails.games, p1Wins: userOneDetails.wins, p1Draws: userOneDetails.draws, p1Choice: userOneDetails.choice });
+      });
+
+    firebase
+      .database()
+      .ref('/users/' + userTwo)
+      .on('value', snapshot => {
+        const userTwoDetails = snapshot.val();
+        this.setState({ p2Email: userTwoDetails.email, p2Games: userTwoDetails.games, p2Wins: userTwoDetails.wins, p2Draws: userTwoDetails.draws, p2Choice: userTwoDetails.choice });
+      });
   }
 
-  updateChoice = (e) => {
-    Alert.alert(e.target.id);
-    // this.setState({ p1Choice: e.target.id });
+  updateChoice = (choice) => {
+    // Alert.alert(e.target.id);
+    this.setState({ p1Choice: choice });
+    const user = this.state.currentUser;
+    firebase
+      .database()
+      .ref('users/' + user.uid)
+      .update({ choice: choice })
+      .catch( error => Alert.alert(error) );
   }
 
   render() {
-
     return (
       <View style={styles.container}>
         <Text>
@@ -65,9 +76,8 @@ export default class Game extends React.Component {
         <Picker
           // selectedValue={this.state.p1Choice}
           style={{height: 50, width: 100}}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({p1Choice: Number(itemValue)})
-          }>
+          onValueChange={ (itemValue) => this.updateChoice(itemValue) }
+        >
           <Picker.Item label="Default" value="0" />
           <Picker.Item label="Foot" value="1" />
           <Picker.Item label="Nuke" value="2" />
