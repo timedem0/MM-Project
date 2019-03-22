99391a614f9e57aebf5ce7f874791bf59efee9ec
Tudor <timedemo@gmail.com>
2019-03-23 00:19:15 +0200

    Add Gyro Component

--- components/Game.js
+++ components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, Picker } from 'react-native';
 import * as firebase from 'firebase';
+import Gyro from './Gyro';
 
 export default class Game extends React.Component {
 
@@ -14,15 +15,22 @@ export default class Game extends React.Component {
   }
 
   componentDidMount() {
+
+    // get the users id
     const { navigation } = this.props;
     const userOne = firebase.auth().currentUser;
     const userTwo = navigation.getParam('data', 'NO-ID');
     this.p1 = firebase.database().ref('/users/' + userOne.uid);
     this.p2 = firebase.database().ref('/users/' + userTwo);
+
+    // start the listeners
     this.getUsersData();
+
+    // player 1 has joined a game
     this.joinGame();
   }
 
+  // navbar maintenance
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'Gaming Arena',
@@ -31,16 +39,21 @@ export default class Game extends React.Component {
   }
 
   getUsersData = () => {
+
+    // player 1 listener
     this.p1.on('value', snapshot => {
       const userOneDetails = snapshot.val();
       this.setState({ p1Email: userOneDetails.email, p1Games: userOneDetails.games, p1Wins: userOneDetails.wins, p1Draws: userOneDetails.draws, p1Choice: userOneDetails.choice, });
     });
+
+    // player 2 listener
     this.p2.on('value', snapshot => {
       const userTwoDetails = snapshot.val();
       this.setState({ p2Email: userTwoDetails.email, p2Games: userTwoDetails.games, p2Wins: userTwoDetails.wins, p2Draws: userTwoDetails.draws, p2Choice: userTwoDetails.choice, p2Playing: userTwoDetails.playing, });
     });
   }
 
+  // update player 1 status in the database as joined a game
   joinGame = () => {
     const userOne = firebase.auth().currentUser;
     firebase
@@ -50,6 +63,7 @@ export default class Game extends React.Component {
       .catch( error => Alert.alert(error) );
   }
 
+  // get player 1 choice, update the state and the database
   updateChoice = (choice) => {
     const userOne = firebase.auth().currentUser;
     const choiceAsNumber = Number(choice);
@@ -61,6 +75,7 @@ export default class Game extends React.Component {
       .catch( error => Alert.alert(error) );
   }
 
+  // database maintenance for clean player exit from game
   leaveGame = () => {
     const userOne = firebase.auth().currentUser;
     firebase
@@ -71,13 +86,16 @@ export default class Game extends React.Component {
     this.props.navigation.navigate('Main');
   }
 
+  // stop the listeners before unmount
   componentWillUnmount() {
     this.p1.off();
     this.p2.off();
   }
 
   render() {
+
     const p2Playing = this.state.p2Playing;
+    
     return (
       <View style={styles.container}>
         <Text>
@@ -92,19 +110,7 @@ export default class Game extends React.Component {
         <Text> </Text>
         { p2Playing
           ? <View>
-              <Text>
-                Your Choice!
-              </Text>
-              <Picker
-                // selectedValue={this.state.p1Choice}
-                style={{height: 50, width: 100}}
-                onValueChange={ (itemValue) => this.updateChoice(itemValue) }
-              >
-                <Picker.Item label="Default" value="0" />
-                <Picker.Item label="Foot" value="1" />
-                <Picker.Item label="Nuke" value="2" />
-                <Picker.Item label="Cockroach" value="3" />
-              </Picker>
+              <Gyro updateChoice={this.updateChoice} />
             </View>
           : <Text>Opponent is not in the game!</Text>
         }
@@ -118,7 +124,9 @@ export default class Game extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: 30,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
   }
 })
---
+++ components/Gyro.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Gyroscope } from 'expo';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class Gyro extends React.Component {
+
+  state = {
+    gyroscopeData: {},
+    south: 0, east: 0, west: 0,
+    southColor: 'rgba(155, 225, 155, 0)', eastColor: 'rgba(155, 225, 155, 0)', westColor: 'rgba(155, 225, 155, 0)',
+  };
+
+  componentDidMount() {
+    this.callGyro();
+  }
+
+  callGyro = () => {
+
+    // add listener to Gyro component
+    this.gyro = Gyroscope.addListener(result => {
+
+      // pass the incoming data stream to state
+      this.setState({ gyroscopeData: result });
+
+      // calculate amplitude of the desired 3 rotations
+      this.state.south = this.state.south + this.state.gyroscopeData.x;
+      this.state.east = this.state.east + this.state.gyroscopeData.y;
+      this.state.west = this.state.west - this.state.gyroscopeData.y;
+
+      // set the color accordingly
+      this.state.southColor = `rgba(155, 225, 155, ${this.state.south/10})`;
+      this.state.eastColor = `rgba(155, 225, 155, ${this.state.east/10})`;
+      this.state.westColor = `rgba(155, 225, 155, ${this.state.west/10})`;
+
+      // calculate the thresholds for applying the choice
+      if (this.state.south >= 10) {
+        this.props.updateChoice(1);
+      } else if (this.state.east >= 10) {
+        this.props.updateChoice(2);
+      } else if (this.state.west >= 10) {
+        this.props.updateChoice(3);
+      }
+    });
+  };
+
+  // reset amplitudes and choice, if needed
+  reset = () => {
+    this.setState({ south: 0, east: 0, west: 0, });
+    this.props.updateChoice(0);
+  }
+
+  // stop the listener before unmount
+  componentWillUnmount() {
+    this.gyro && this.gyro.remove();
+    this.gyro = null;
+  }
+
+  render() {
+
+    return (
+      <View>
+
+        <View style={styles.shapeContainer}>
+          <View style={[styles.trapezoid, {borderBottomColor: this.state.westColor, transform: [{rotate: '90deg'}]}]}><Text>{"\n\n"}FOOT</Text></View>
+          <View style={[styles.trapezoid, {borderBottomColor: this.state.southColor, marginTop: 30}]}><Text>{"\n\n"}NUKE</Text></View>
+          <View style={[styles.trapezoid, {borderBottomColor: this.state.eastColor, transform: [{rotate: '270deg'}]}]}><Text>{"\n\n"}ROACH</Text></View>
+        </View>
+
+        <View style={{marginTop: 30}}>
+          <Button title="Reset" onPress={this.reset} />
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  shapeContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    marginTop: 50,
+  },
+  trapezoid: {
+    width: 100,
+    height: 0,
+    borderBottomWidth: 50,
+    borderBottomColor: 'red',
+    borderLeftWidth: 25,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 25,
+    borderRightColor: 'transparent',
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
